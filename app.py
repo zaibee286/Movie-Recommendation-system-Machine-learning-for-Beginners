6fd54abd2ad16cd03f6497192569ebb13195dce1
Khan zaib <168482097+zaibee286@users.noreply.github.com>
2026-01-02 21:53:29 +0500

    Update app.py
    now you can see the poster  of the movies

--- app.py
+++ app.py
@@ -1,19 +1,84 @@
+from unicodedata import name
+import requests
 import streamlit as st
 import pickle
+import pandas as pd
 
-movies_list = pickle.load(open('practice areana/movies_list.pkl', 'rb'))
-similarity = pickle.load(open('practice areana/similarity.pkl', 'rb'))
 
-st.title('Movie Recommendation System')
+import streamlit as st
 
-selected_movie = st.selectbox(
-    'Select a movie:', movies_list['title'].values)
-if st.button('Recommend'):
-    index = movies_list[movies_list['title'] == selected_movie].index[0]
-    distances = similarity[index]
-    movie_list = sorted(
-        list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+st.set_page_config(
+    page_title="Movie Recomendation App",
+    page_icon="🎥",
+    
+)
 
-    st.subheader('Top 5 movie recommendations:')
-    for i in movie_list:
-        st.write(movies_list.iloc[i[0]].title)
+
+def fetch_poster(movie_id):
+    response=requests.get("https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id))
+    data=response.json()
+    return "https://image.tmdb.org/t/p/w500/"+data['poster_path']
+    
+
+def recommend(movie):
+    movie_index=movies[movies["title"]==movie].index[0]
+    distances=similarity[movie_index]
+    movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+    recommend_moives=[]
+    movie_id=[]
+    for i in movies_list:
+        movie_id.append(movies.iloc[i[0]].movie_id)
+        recommend_moives.append(movies.iloc[i[0]].title)
+    return recommend_moives, movie_id
+   
+
+###   set your files path here   ###
+
+movies_list=pickle.load(open("movies_list.pkl","rb"))
+movies=pd.DataFrame(movies_list)
+
+similarity=pickle.load(open("similarity.pkl","rb")) 
+
+
+
+
+
+st.title("Movie Recommendation System")
+
+
+def rcmd(id):
+
+    name = st.selectbox("Search here !",movies["title"].values,)
+    current_movie_id=movies[movies["title"]==name].iloc[0].movie_id
+    
+
+   
+
+    st.image(fetch_poster(current_movie_id), caption=name)    
+    if st.button("RECOMMENDATIONS"):
+
+        recom, movie_ids = recommend(name)
+    
+    
+
+
+        col1, col2, col3, col4, col5 = st.columns(5)
+        with col1:
+            st.text(recom[0])
+            st.image(fetch_poster(movie_ids[0]))
+
+        with col2:
+            st.text(recom[1])
+            st.image(fetch_poster(movie_ids[1]))
+
+        with col3:
+            st.text(recom[2])
+            st.image(fetch_poster(movie_ids[2]))
+        with col4:
+            st.text(recom[3])
+            st.image(fetch_poster(movie_ids[3]))
+        with col5:
+            st.text(recom[4])
+            st.image(fetch_poster(movie_ids[4]))
+rcmd(name)
+        
